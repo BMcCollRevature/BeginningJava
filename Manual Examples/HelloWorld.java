a24ad62711666077702926e5ab2f5c1ed8c164e4
BrigitteMcColl <bmccoll@csu.fullerton.edu>
2021-07-14 15:28:28 -0700

    adding manual examples:

---
+++ Manual Examples/HelloWorld.java
@@ -0,0 +1,10 @@
+/* a class file in java can only contain a single public class. the public 
+class must have the same name as the .java file itself */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Main methods are required if you want to perform action with java class file
+		System.out.println("Hello World!");
+	}
+}
