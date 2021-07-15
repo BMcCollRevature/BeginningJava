5f36de8581a4f763b524cf2b3da5256cf7e5add6
BrigitteMcColl <bmccoll@csu.fullerton.edu>
2021-07-15 16:06:16 -0700

    Adding Lab - Common Strings Method

---
+++ Labs/Fridays-Labs/Lab - Common String Methods/src/commonString/StringMethods.java
@@ -0,0 +1,15 @@
+package commonString;
+
+public class StringMethods {
+	
+	public static void main(String[] args) {
+		String str = "Hello";
+		System.out.println(str.equals("Hello"));
+		
+		// length
+		System.out.println(str.length());
+		
+		//index of
+		System.out.println(str.indexOf("H"));
+	}
+}
